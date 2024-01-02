5fc013a02b7e83ed673471c2147ba4cdbf95f61c
cissyan <92156590+CissyYan@users.noreply.github.com>
2024-01-02 15:48:34 +0800

    Create JJHL.js

---
+++ JJHL.js
@@ -0,0 +1,11 @@
+# 应用名称：极简汇率
+# 脚本作者：ddgksf2013
+# 微信账号：墨鱼手记
+# 解锁说明：解锁钻石会员权限
+# https://raw.githubusercontent.com/ddgksf2013/MoYu/master/JiJianHuiLvProCrack.js
+
+var ddgksf2013 = JSON.parse($response.body);
+ddgksf2013.data=[{vipStatus:"paid",vipLevel:"senior",expire:"2099-12-18 10:10:00",vipPayType:"auto_sub",vipPayUnit:"year",vipPayNum:1}];
+$done({
+    body: JSON.stringify(ddgksf2013),
+});
