70edc3930577af119ca76364f6aa2fffa52a39b9
cissyan <92156590+CissyYan@users.noreply.github.com>
2024-01-05 09:19:28 +0800

    Delete JJHL.js

--- JJHL.js
+++
@@ -1,5 +0,0 @@
-var ddgksf2013 = JSON.parse($response.body);
-ddgksf2013.data=[{vipStatus:"paid",vipLevel:"senior",expire:"2099-12-18 10:10:00",vipPayType:"auto_sub",vipPayUnit:"year",vipPayNum:1}];
-$done({
-    body: JSON.stringify(ddgksf2013),
-});
